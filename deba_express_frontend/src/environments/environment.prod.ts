1b14b9426c7c73416477ceee74ad6182f8dcd7cb
Frank Tonya <francktonya7@gmail.com>
2026-08-16 03:41:14 +0200

    fix: configuration stricte de l'environnement de production et injection de l'API

--- deba_express_frontend/src/environments/environment.prod.ts
+++ deba_express_frontend/src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://vercel.app' // Adresse de déploiement en production
+  apiUrl: 'https://deba-express-backend.vercel.app/' // Adresse de déploiement en production
 };
